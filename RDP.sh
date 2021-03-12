c276578f7c8d5b2058242cf6613a9b534218312e
alexporumbel <65975118+alexporumbel@users.noreply.github.com>
2021-03-12 22:59:35 +0200

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
+printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m ALOK
 sudo adduser ALOK sudo
@@ -17,19 +17,18 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.2-1.deb
-sudo dpkg -i crossover_20.0.2-1.deb
-sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo apt -y install obs-studio
-sudo apt -y install firefox
 sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
-printf "\nSetup Completed " >&2 ||
+printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
 su - ALOK -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n'
-printf 'Your SUDO Pasword Is 8426 \n'
-printf 'Terminal issue solve - https://youtu.be/XZxxaNWJxT0'
+printf 'Check https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+else
+    printf "\n\nError Occured " >&2
+fi
